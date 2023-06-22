de386b846832913c6194de196a6a45c60a868c18
Gael Hofman <Gieltie@outlook.fr>
2023-06-22 22:14:20 +0200

    render all the feed and used fa icons

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 import { tweetsData } from "./data.js";
-/*
-Challenge:
-1. Put all of the data in its own file called
-   data.js, and export it back into index.js. 
-   Make any changes to index.html that are
-   necessary to make this work.
-2. Log out tweetsData.
-*/
-console.log(tweetsData)
+
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
+
+tweetBtn.addEventListener('click', () => {
+    console.log(tweetInput.value)
+})
+
+function getFeedHtml(){
+    let feedHtml = ``
+tweetsData.forEach(function(tweet){
+    feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                <i class="fa-regular fa-comment-dots"></i>
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                <i class="fa-solid fa-heart"></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                <i class="fa-solid fa-retweet"></i>
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+` 
+    })
+    return feedHtml
+}
+
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
+
+render()
