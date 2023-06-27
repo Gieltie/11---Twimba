3eb0c226f0ec0310b50a15343d2c7f8fd38af73f
Gael Hofman <Gieltie@outlook.fr>
2023-06-27 21:25:06 +0200

    like and share button working with css class

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-import { tweetsData } from "./data.js";
-
+import { tweetsData } from './data.js'
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
 
-tweetBtn.addEventListener('click', () => {
+tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value)
 })
-document.addEventListener('click', (e) =>{
-    if(e.target.dataset.reply){
-        console.log(e.target.dataset.reply)
-    } else if(e.target.dataset.like) {
-        handleLikeClick(e.target.dataset.like)
-    } else if(e.target.dataset.retweet) {
-        console.log(e.target.dataset.retweet)
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like){
+       handleLikeClick(e.target.dataset.like) 
+    }
+    else if(e.target.dataset.retweet){
+        handleRetweetClick(e.target.dataset.retweet)
     }
 })
 
@@ -20,19 +19,49 @@ function handleLikeClick(tweetId){
     const targetTweetObj = tweetsData.filter(function(tweet){
         return tweet.uuid === tweetId
     })[0]
+
     if (targetTweetObj.isLiked){
         targetTweetObj.likes--
-    } else {
-        targetTweetObj.likes++
+    }
+    else{
+        targetTweetObj.likes++ 
     }
     targetTweetObj.isLiked = !targetTweetObj.isLiked
     render()
 }
 
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+    
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }
+    else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    render() 
+}
+
 function getFeedHtml(){
     let feedHtml = ``
-tweetsData.forEach(function(tweet){
-    feedHtml += `
+    
+    tweetsData.forEach(function(tweet){
+        
+        let likeIconClass = ''
+        
+        if (tweet.isLiked){
+            likeIconClass = 'liked'
+        }
+        
+        let retweetIconClass = ''
+        if (tweet.isRetweeted){
+            retweetIconClass = 'retweeted'
+        }
+          
+        feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
         <img src="${tweet.profilePic}" class="profile-pic">
@@ -41,24 +70,30 @@ tweetsData.forEach(function(tweet){
             <p class="tweet-text">${tweet.tweetText}</p>
             <div class="tweet-details">
                 <span class="tweet-detail">
-                <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
+                    <i class="fa-regular fa-comment-dots"
+                    data-reply="${tweet.uuid}"
+                    ></i>
                     ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
-                <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
+                    <i class="fa-solid fa-heart ${likeIconClass}"
+                    data-like="${tweet.uuid}"
+                    ></i>
                     ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
-                <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
+                    <i class="fa-solid fa-retweet ${retweetIconClass}"
+                    data-retweet="${tweet.uuid}"
+                    ></i>
                     ${tweet.retweets}
                 </span>
             </div>   
         </div>            
     </div>
 </div>
-` 
-    })
-    return feedHtml
+`
+   })
+   return feedHtml 
 }
 
 function render(){
